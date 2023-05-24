f88d1747d4d0dda9297b7c36d7d0c5ee0b98ee6d
tuantt7 <tranthanhtuan62@gmail.com>
2023-05-24 16:43:32 +0700

    Change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,26 @@
 <template>
-  <no-ssr>
-    <div>
-      <client-only>
-        <template v-if="!errorCode">
-          <el-menu
-            default-active="/address/0x00fDe51cC2EE327F2cB7f85EF13947f4d6E4574F"
-            class="el-menu-demo"
-            mode="horizontal"
-            :router="true"
-          >
-            <el-menu-item
-              index="/address/0x00fDe51cC2EE327F2cB7f85EF13947f4d6E4574F"
-              >Home</el-menu-item
-            >
-          </el-menu>
-          <Nuxt
-        /></template>
-        <template v-else>
-          <div class="container">
-            <h1>Page not found</h1>
-            <h1>{{ ERROR_CODE[errorCode] }}</h1>
-          </div>
-        </template>
-      </client-only>
-    </div>
-  </no-ssr>
+  <div>
+    <template v-if="!errorCode">
+      <el-menu
+        default-active="/address/0x00fDe51cC2EE327F2cB7f85EF13947f4d6E4574F"
+        class="el-menu-demo"
+        mode="horizontal"
+        :router="true"
+      >
+        <el-menu-item
+          index="/address/0x00fDe51cC2EE327F2cB7f85EF13947f4d6E4574F"
+          >Home</el-menu-item
+        >
+      </el-menu>
+      <Nuxt
+    /></template>
+    <template v-else>
+      <div class="container">
+        <h1>Page not found</h1>
+        <h1>{{ ERROR_CODE[errorCode] }}</h1>
+      </div>
+    </template>
+  </div>
 </template>
 <script>
 import { mapState } from 'vuex'
